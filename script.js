3cb931f9e93ba1bdf3067b611ab5a22b58b1bfd6
MD Mahir Faysal <mahirfaysal28@gmail.com>
2024-08-06 17:23:15 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript to add interactivity
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('alertButton');
+    
+    button.addEventListener('click', () => {
+        alert('Button clicked!');
+    });
+});
